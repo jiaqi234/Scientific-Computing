04aad1e5f4ab25a06bb7a85e9e6121659e584e57
Jiaqi <jiaqi@eduroam-132-58.nomadic.bris.ac.uk>
2022-03-21 16:45:56 +0000

    Update

--- shootingtesting.py
+++ shootingtesting.py
@@ -3,45 +3,42 @@ from scipy.optimize import fsolve
 import numpy as np
 import odesolver
 
-def hopf(u0,t):
-    beta = 1
-    sigma = -1
-    du1dt = beta*u0[0]-u0[1]+ (u0[0]**2+u0[1]**2) * sigma
-    du2dt = u0[0] + beta*u0[1]+(u0[0]**2+u0[1]**2)* sigma
+def hopf(u0,t,args):
+    du1dt = args[0]*u0[0]-u0[1]+ (u0[0]**2+u0[1]**2) * args[1]
+    du2dt = u0[0] + args[0]*u0[1]+(u0[0]**2+u0[1]**2)* args[1]
     du3dt = -u0[2]
     return np.array([du1dt,du2dt,du3dt])
 
 
-def hopf_sol(t, phase):
-    beta = 1
-    u1 = np.sqrt(beta) * np.cos(t + phase)
-    u2 = np.sqrt(beta) * np.sin(t + phase)
+def hopf_sol(t,phase,args):
+    u1 = np.sqrt(args[0]) * np.cos(t + phase)
+    u2 = np.sqrt(args[0]) * np.sin(t + phase)
     u3 = np.exp(-(t + phase))
     return np.array([u1, u2, u3])
 
-def phase_condition(f,u0):
-    return f(u0,1)[0]
-
 def shoot(f):
-    def shooting(u0):
-        t = np.linspace(0,u0[-1],100)
-        sol = odesolver.solve_ode(odesolver.rk4_step,f,u0[:-1],t,0.01)
-        return np.append(u0[:-1]- sol[:,-1],phase_condition(f,u0[:-1]))
+    def shooting(u0,pc,*args):
+        t = np.linspace(0,u0[-1],1000)
+        sol = odesolver.solve_ode(odesolver.rk4_step,f,u0[:-1],t,0.01,*args)
+        return np.append(u0[:-1]- sol[:,-1],pc(u0,*args))
     return shooting
 
-def limit_cycle(f,u0):
-    return fsolve(shoot(f),u0)
+def limit_cycle(f,pc,u0,*args):
+    return fsolve(shoot(f),u0,args = (pc,*args))
+
+def phase_condition(u0,args):
+    return hopf(u0,1,args)[0]
 
 t = np.linspace(0, 20, 50)
 u0 = np.array([1.2, 0.3, 1.2, 10])
-sol = odesolver.solve_ode(odesolver.rk4_step,hopf,u0[:-1],t,0.01)
-orbit = limit_cycle(hopf,u0)
+sol = odesolver.solve_ode(odesolver.rk4_step,hopf,u0[:-1],t,0.01,[2,-1])
+orbit = limit_cycle(hopf,phase_condition,u0,[2,-1])
 ax = plt.axes(projection='3d')
 ax.plot(sol[0],sol[1],sol[2])
 ax.scatter(orbit[0], orbit[1],orbit[2],'go', label="Numerical shooting point")
 plt.show()
 
-if np.allclose(hopf_sol(0,orbit[-1]),orbit[:-1]):
+if np.allclose(hopf_sol(0,orbit[-1],[2,-1]),orbit[:-1]):
     print("Test passed")
 else:
     print("Test failed")
