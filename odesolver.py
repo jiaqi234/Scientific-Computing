d4401ffde9aef017770173e7c1c36ad388109228
Jiaqi <jiaqi@eduroam-132-58.nomadic.bris.ac.uk>
2022-03-21 16:12:33 +0000

    Update odesolver

--- odesolver.py
+++ odesolver.py
@@ -6,32 +6,32 @@ def f(u,t):
     dydt = -u[0]
     return np.array([dxdt,dydt])
 
-def euler_step(f,x0,t0,n):
-    xn = x0 + n * f(x0,t0)
+def euler_step(f,x0,t0,n,*args):
+    xn = x0 + n * f(x0,t0,*args)
     tn = t0 + n
     return xn,tn
 
-def rk4_step(f,x0, t0, n):
-    k1 = f(x0,t0)
-    k2 = f(x0 + 0.5 * n * k1, t0 + 0.5 * n)
-    k3 = f(x0 + 0.5 * n * k2, t0 + 0.5 * n)
-    k4 = f(x0 + n * k3, t0 + n)
-    xn = x0 + n* (k1 + 2 * k2 + 2 * k3 + k4)/6
+def rk4_step(f,x0, t0, n,*args):
+    k1 = f(x0,t0,*args)
+    k2 = f(x0 + 0.5 * n * k1, t0 + 0.5 * n,*args)
+    k3 = f(x0 + 0.5 * n * k2, t0 + 0.5 * n,*args)
+    k4 = f(x0 + n * k3, t0 + n,*args)
+    xn = x0 + n * (k1 + 2 * k2 + 2 * k3 + k4)/6
     tn = t0 + n
     return xn,tn
 
-def solve_to(method,f,x0,t0,tn,deltat_max):
+def solve_to(method,f,x0,t0,tn,deltat_max,*args):
     while tn - t0 > deltat_max:
-        x0,t0 = method(f,x0,t0,deltat_max)
+        x0,t0 = method(f,x0,t0,deltat_max, *args)
     else:
-        x0,t0 = method(f,x0,tn-t0,deltat_max)
+        x0,t0 = method(f,x0,t0,tn-t0,*args)
     return x0
 
-def solve_ode(method,f,x0,t,deltat_max):
+def solve_ode(method,f,x0,t,deltat_max,*args):
     x = np.empty(shape = (len(t),len(x0)))
     x[0] = x0
     for i in range (len(t)-1):
-        x[i+1] = solve_to(method,f,x[i],t[i],t[i+1],deltat_max)
+        x[i+1] = solve_to(method,f,x[i],t[i],t[i+1],deltat_max, *args)
     return x.transpose()
 
 # t = np.linspace(0,20,100)
